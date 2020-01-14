bc0b27dc74f0c86e36ed70dd4be38c7dbe1fa107
Darya Kunickaya <darya.kunickaya@gmail.com>
2020-01-14 13:25:09 +0300

    Task complete

--- Task1/Population.cs
+++ Task1/Population.cs
@@ -6,8 +6,22 @@ namespace Task1
     {
         public static int GetYears(int initialPopulation, double percent, int visitors, int currentPopulation)
         {
-            // put your code here
-            throw new NotImplementedException();
+            if (currentPopulation <= 0 || initialPopulation <= 0 || visitors <= 0)
+            {
+                throw new ArgumentException("Invalid parameters of population or visitors");
+            }
+            if (percent < 0 || percent > 100)
+            {
+                throw new ArgumentOutOfRangeException("Invalid parameters of percent");
+            }
+            int years = 0;
+            while (initialPopulation <= currentPopulation)
+            {
+                double newPop = initialPopulation * (1 + percent / 100) + visitors;
+                initialPopulation = Convert.ToInt32(newPop);
+                years++;
+            }
+            return years;
         }
     }
 }

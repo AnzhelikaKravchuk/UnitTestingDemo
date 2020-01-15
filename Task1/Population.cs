d6cfd77b664ac112d5fbcc5c55b0ba64c2d82f17
andrjak <79821843rt@gmail.com>
2020-01-15 20:02:48 +0300

    refactor: add XML-documentation

--- Task1/Population.cs
+++ Task1/Population.cs
@@ -2,11 +2,34 @@
 
 namespace Task1
 {
+    /// <summary>
+    /// The main class of the project.
+    /// </summary>
     public static class Population
     {
+        /// <summary>
+        /// Counts how many years will pass before the population reaches the desired size.
+        /// </summary>
+        /// <param name="initialPopulation">The initial number of settlers. </param>
+        /// <param name="percent">Population growth rate percent. </param>
+        /// <param name="visitors">The number of people coming every year. </param>
+        /// <param name="currentPopulation">Required size of population. </param>
+        /// <returns>Сount of years. </returns>
+        /// <exception cref="System.ArgumentOutOfRangeException">
+        /// Occurs when percent values are out of the range of 0 to 100.
+        /// </exception>
+        /// <exception cref="System.ArgumentException">
+        /// Occurs when initialPopulation less or equal zero or
+        /// count of visitors less than zero or
+        /// current population less or equals zero.
+        /// </exception>
+        /// <example>
+        /// <code>
+        /// int year = GetYears(1500, 5, 100, 5000)
+        /// </code>
+        /// </example>
         public static int GetYears(int initialPopulation, double percent, int visitors, int currentPopulation)
         {
-            // put your code here
             if (initialPopulation <= 0)
             {
                 throw new ArgumentException("Initial population cannot be less or equals zero.");

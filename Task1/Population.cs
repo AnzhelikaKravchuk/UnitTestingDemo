d49455693fb43fa508a2959925e54707fc9a3c1b
Constantin Hitov <kostya.khitov@mail.ru>
2020-01-16 12:44:18 +0300

    Complete Task1 and Add code-analysis.ruleset

--- Task1/Population.cs
+++ Task1/Population.cs
@@ -6,8 +6,27 @@ namespace Task1
     {
         public static int GetYears(int initialPopulation, double percent, int visitors, int currentPopulation)
         {
-            // put your code here
-            throw new NotImplementedException();
+            if (initialPopulation <= 0)
+                throw new ArgumentException("Initial population cannot be less or equals zero.");
+
+            if (percent < 0 || percent > 100)
+                throw new ArgumentOutOfRangeException("Value of percents cannot be less than 0% or more than 100%.");
+
+            if (visitors < 0)
+                throw new ArgumentException("Count of visitors cannot be less zero.");
+
+            if (currentPopulation <= 0)
+                throw new ArgumentException("Current population cannot be less or equals zero.");
+
+            int years = 0;
+            double population = initialPopulation;
+
+            while(population < currentPopulation)
+            {
+                population = population * (1 + percent / 100) + visitors;
+                years++;
+            }
+            return years;
         }
     }
 }

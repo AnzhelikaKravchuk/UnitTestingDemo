a0ba31c65965758e2545d37215c0f3cbb1ef64b4
Constantin Hitov <kostya.khitov@mail.ru>
2020-01-16 12:56:12 +0300

    Add StyleCop and FxCopAnalyzers

--- Task1/Population.cs
+++ Task1/Population.cs
@@ -7,25 +7,34 @@ namespace Task1
         public static int GetYears(int initialPopulation, double percent, int visitors, int currentPopulation)
         {
             if (initialPopulation <= 0)
+            {
                 throw new ArgumentException("Initial population cannot be less or equals zero.");
+            }
 
             if (percent < 0 || percent > 100)
-                throw new ArgumentOutOfRangeException("Value of percents cannot be less than 0% or more than 100%.");
+            {
+                throw new ArgumentOutOfRangeException(nameof(percent), "Value of percents cannot be less then 0% or more then 100%.");
+            }
 
             if (visitors < 0)
+            {
                 throw new ArgumentException("Count of visitors cannot be less zero.");
+            }
 
             if (currentPopulation <= 0)
+            {
                 throw new ArgumentException("Current population cannot be less or equals zero.");
+            }
 
             int years = 0;
             double population = initialPopulation;
 
-            while(population < currentPopulation)
+            while (population < currentPopulation)
             {
-                population = population * (1 + percent / 100) + visitors;
+                population = (population * (1 + (percent / 100))) + visitors;
                 years++;
             }
+
             return years;
         }
     }

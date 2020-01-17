78d5ef4283b6bc00aea0f20af2cc9b5cbdb42471
Tatuana4444 <47916561+Tatuana4444@users.noreply.github.com>
2020-01-17 17:31:17 +0300

    Implementation of the GetYears method

--- Task1/Population.cs
+++ Task1/Population.cs
@@ -2,12 +2,50 @@
 
 namespace Task1
 {
+    /// <summary>
+    /// Class that discribeы population.
+    /// </summary>
     public static class Population
     {
+        /// <summary>
+        /// Сonsiders how many years the town needs to see its population greater or equal to currentPopulation inhabitants.
+        /// </summary>
+        /// <param name="initialPopulation">the population at the beginning of a year.</param>
+        /// <param name="percent">number of regular increase in residents.</param>
+        /// <param name="visitors">new inhabitants per year.</param>
+        /// <param name="currentPopulation">current population inhabitants.</param>
+        /// <returns>How many years does the town need to see its population greater or equal to currentPopulation inhabitants.</returns>
         public static int GetYears(int initialPopulation, double percent, int visitors, int currentPopulation)
         {
-            // put your code here
-            throw new NotImplementedException();
+            if (initialPopulation <= 0)
+            {
+                throw new ArgumentException("Initial population cannot be less or equals zero.");
+            }
+
+            if (currentPopulation <= 0)
+            {
+                throw new ArgumentException("Current population cannot be less or equals zero.");
+            }
+
+            if (visitors < 0)
+            {
+                throw new ArgumentException("Count of visitors cannot be less zero.");
+            }
+
+            if (percent < 0 || percent > 100)
+            {
+                throw new ArgumentOutOfRangeException(nameof(percent), "Value of percents cannot be less then 0% or more then 100%.");
+            }
+
+            double peopleCount = initialPopulation;
+            int years = 0;
+            while (peopleCount < currentPopulation)
+            {
+                years++;
+                peopleCount += (peopleCount * percent / 100) + visitors;
+            }
+
+            return years;
         }
     }
 }

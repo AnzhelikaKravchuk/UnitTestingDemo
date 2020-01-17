7096960dd0f41793568ba451de3db88265ea5672
PashaFast <pashahit@gmail.com>
2020-01-18 02:17:38 +0300

    Complete Task1

--- Task1/Population.cs
+++ Task1/Population.cs
@@ -6,7 +6,17 @@ namespace Task1
     {
         public static int GetYears(int initialPopulation, double percent, int visitors, int currentPopulation)
         {
-            // put your code here
+            if (visitors < 0) throw new ArgumentException("Count of visitors cannot be less zero.");
+            if ((percent > 100) || (percent < 0)) throw new ArgumentOutOfRangeException("Value of percents cannot be less then 0% or more then 100%.");
+            if (initialPopulation <= 0) throw new ArgumentException("Initial population cannot be less or equals zero.");
+            
+            int countOfYears = 0;
+            while (initialPopulation < currentPopulation)
+            {
+                initialPopulation = initialPopulation + visitors + ((int)((initialPopulation * percent) / 100));
+                countOfYears++;
+            }
+            return countOfYears;
             throw new NotImplementedException();
         }
     }

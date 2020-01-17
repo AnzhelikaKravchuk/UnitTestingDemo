e91610dc8c97284a96375a89110e92e5d788f57a
Zhenya Salash <ev.salash@gmail.com>
2020-01-17 18:43:41 +0300

    I corrected the task.

--- Task1/Population.cs
+++ Task1/Population.cs
@@ -25,17 +25,27 @@ namespace Task1
         /// <returns>
         /// How many years meet the requirement.
         /// </returns>
-        /// /// /// <exception cref="System.ArgumentException">
-        /// Initial population cannot be less or equals zero.
+        /// <exception cref="System.ArgumentException">
+        /// Initialpopulation, visitors, currentPopulation cannot be less or equals zero.
         ///  </exception>
-        /// /// <exception cref="System.ArgumentOutOfRangeException">
+        /// <exception cref="System.ArgumentOutOfRangeException">
         /// Value of percents cannot be less then 0% or more then 100%.
         ///  </exception>
         public static int GetYears(int initialPopulation, double percent, int visitors, int currentPopulation)
         {
-            if (initialPopulation <= 0 || visitors <= 0 || currentPopulation <= 0)
+            if (initialPopulation <= 0)
             {
-                 throw new ArgumentException("Initial population cannot be less or equals zero.");
+                throw new ArgumentException("initialPopulation <=0", nameof(initialPopulation));
+            }
+
+            if (visitors <= 0)
+            {
+                throw new ArgumentException("visitors <=0", nameof(visitors));
+            }
+
+            if (currentPopulation <= 0)
+            {
+                throw new ArgumentException("currentPopulation <=0", nameof(currentPopulation));
             }
 
             if (percent < 0 || percent > 100)

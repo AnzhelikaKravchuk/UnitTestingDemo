f2d87a415aaae2f887a8d45ba6ce92d5ed0f9991
Ksenya Rogaleva <ksenyarogaleva87@gmail.com>
2020-01-19 01:37:53 +0300

    Add the completed task of counting the years.

--- Task1/Population.cs
+++ Task1/Population.cs
@@ -6,8 +6,34 @@ namespace Task1
     {
         public static int GetYears(int initialPopulation, double percent, int visitors, int currentPopulation)
         {
-            // put your code here
-            throw new NotImplementedException();
+            if (initialPopulation <= 0)
+            {
+                throw new ArgumentException("Initial population cannot be less or equals zero.", nameof(initialPopulation));
+            }
+
+            if(percent<0 || percent > 100)
+            {
+                throw new ArgumentOutOfRangeException("Value of percents cannot be less then 0% or more then 100%.", nameof(percent));
+            }
+
+            if (visitors < 0)
+            {
+                throw new ArgumentException("Count of visitors cannot be less zero.", nameof(visitors));
+            }
+
+            if (currentPopulation <= 0)
+            {
+                throw new ArgumentException("Current population cannot be less or equals zero.", nameof(currentPopulation));
+            }
+
+            int yearCount = 0;
+            while (initialPopulation < currentPopulation)
+            {
+                initialPopulation += (int)((percent * initialPopulation) / 100 + visitors);
+                yearCount++;
+            }
+
+            return yearCount;
         }
     }
 }

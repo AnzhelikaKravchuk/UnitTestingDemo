f9a25dadd84e47b32f620dba82d1bf947e6cc0e5
GreenAcidy <FirstKnight@list.ru>
2020-01-19 00:06:09 +0300

    Task done

--- Task1/Population.cs
+++ Task1/Population.cs
@@ -1,20 +1,54 @@
-﻿using System;
+﻿// <copyright file="Population.cs" company="PlaceholderCompany">
+// Copyright (c) PlaceholderCompany. All rights reserved.
+// </copyright>
 
 namespace Task1
 {
+    using System;
+    using System.Reflection;
+    using System.Resources;
+
+    /// <summary >
+    /// Сlass Population.
+    /// </summary >
     public static class Population
     {
+        /// <summary >
+        /// Algorith of finding time to reach the current population level.
+        /// </summary >
+        /// <param  name = " initialPopulation " > Initial population value. </param >
+        /// <param  name = " percent " > Population growth rate. </param >
+        /// <param  name = " visitors " > Number of visitors. </param >
+        /// <param  name = " currentPopulation " > Current population. </param >
+        /// <exception cref="System.ArgumentException">Throw when
+        /// initialPopulation or percent or visitors are less than 0 or equal 0.
+        /// </exception>
+        /// <exception cref="System.ArgumentOutOfRangeException">Throw then percent is greater than 100
+        /// or less than 0.
+        /// </exception>
+        /// <returns>Time to reach the current population level.  </returns>
         public static int GetYears(int initialPopulation, double percent, int visitors, int currentPopulation)
         {
             // put your code here
-            if (initialPopulation<=0)
+            if (initialPopulation <= 0)
+            {
                 throw new ArgumentException("Initial population cannot be less or equals zero.");
-            if (percent <= 0 || percent >100)
-                throw new ArgumentOutOfRangeException("Value of percents cannot be less then 0% or more then 100%.");
+            }
+
+            if (percent <= 0 || percent > 100)
+            {
+                throw new ArgumentOutOfRangeException(nameof(percent), "Value of percents cannot be less then 0% or more then 100%.");
+            }
+
             if (visitors < 0)
+            {
                 throw new ArgumentException("Count of visitors cannot be less zero.");
+            }
+
             if (currentPopulation <= 0)
+            {
                 throw new ArgumentException("Current population cannot be less or equals zero.");
+            }
 
             int result = 0;
             while (currentPopulation > initialPopulation)
@@ -22,10 +56,9 @@ namespace Task1
                 initialPopulation += (int)(initialPopulation * percent / 100) + visitors;
                 result++;
             }
+
             return result;
             throw new NotImplementedException();
-
         }
-        
     }
 }
